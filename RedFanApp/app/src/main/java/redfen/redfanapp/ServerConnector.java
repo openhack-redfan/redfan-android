f3effcc316783fdd7e0d1b31e9d74ad7fc822798
Mingyu Jo <owlsogul@naver.com>
2018-06-29 01:36:59 +0900

    and server: Add communicate with POST method

--- RedFanApp/app/src/main/java/redfen/redfanapp/ServerConnector.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/ServerConnector.java
@@ -70,5 +70,52 @@ public class ServerConnector {
         });
     }
 
+    /**
+     * Url 주소와 RequestCallback 인터페이스를 구현하여 전달하면,
+     * 해당 주소에서 Response를 받아와 RequestCallback의 requestCallback 메소드의
+     * result 로 전달한다.
+     * @param url 요청할 페이지의 url 주소
+     * @param data POST 방식으로 전달할 data
+     * @param callback Response 정보로 콜백을 한다.
+     */
+    public void requestPost(final String url, final String data, final RequestCallback callback){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                URL endpoint = null;
+                try {
+                    endpoint = new URL(url);
+                    HttpsURLConnection conn = (HttpsURLConnection) endpoint.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setDoOutput(true);
+                    conn.getOutputStream().write(data.getBytes());
+                    if (conn.getResponseCode() == 200){ // 네트워크 연결에 성공했을 경우
+
+                        // 인풋 가져옴
+                        InputStream is = conn.getInputStream();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        StringBuilder sbr = new StringBuilder(); // 읽어온 스트링 저장하는 스트링빌더
+                        String line = null; // 한줄씩 들어가는 임시 보관소
+                        while ((line = br.readLine()) != null){
+                            sbr.append(line);
+                        }
+
+                        br.close();
+                        is.close();
+
+                        // callback 함수 호출
+                        callback.requestCallback(sbr.toString());
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
 
 }
