c001a8b51d055b4ae54848ad1564f50a3e006807
Mingyu Jo <owlsogul@naver.com>
2018-06-29 01:49:42 +0900

    and gui: Add comment at IntroActivity

--- RedFanApp/app/src/main/java/redfen/redfanapp/IntroActivity.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/IntroActivity.java
@@ -5,6 +5,12 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Intro Activity
+ * 처음 실행할 때, 유저들에게 회원가입을 하도록 유도하는 설명서를 표시한다.
+ * 아닐 경우 로딩화면만 보여주고 넘어가도록 한다.
+ * Created By JoMingyu
+ */
 public class IntroActivity extends AppCompatActivity {
 
     @Override
