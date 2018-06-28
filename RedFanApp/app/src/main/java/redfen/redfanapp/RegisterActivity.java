feb0e5867c8e8c3a1bb35173d8d7c2ad378c3b3a
Mingyu Jo <owlsogul@naver.com>
2018-06-29 02:32:09 +0900

    and gui: just create register activity

---
+++ RedFanApp/app/src/main/java/redfen/redfanapp/RegisterActivity.java
@@ -0,0 +1,16 @@
+package redfen.redfanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 회원가입을 위한 액티비티 입니다.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
