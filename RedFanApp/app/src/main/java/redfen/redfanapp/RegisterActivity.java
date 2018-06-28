ca55f9d39186c4792a5b9cd0b8bb801b247746b3
Mingyu Jo <owlsogul@naver.com>
2018-06-29 03:03:26 +0900

    and gui: fake register(sign up)

--- RedFanApp/app/src/main/java/redfen/redfanapp/RegisterActivity.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/RegisterActivity.java
@@ -1,16 +1,47 @@
 package redfen.redfanapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * 회원가입을 위한 액티비티 입니다.
  */
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputPasswordCheck;
+
+    private TextView btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        inputPasswordCheck = (EditText) findViewById(R.id.inputPasswordCheck);
+
+        btnLogin = (TextView) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoginClick(v);
+            }
+        });
+
+    }
+
+    public void onRegisterClick(View view){
+        this.finish();
+    }
+
+    public void onLoginClick(View view){
+        this.finish();
     }
 }
