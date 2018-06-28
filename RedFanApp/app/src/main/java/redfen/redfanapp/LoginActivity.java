e5b52c281b21443976b4943ee001f223a5960368
Mingyu Jo <owlsogul@naver.com>
2018-06-29 02:28:16 +0900

    and gui: fake login 구현

--- RedFanApp/app/src/main/java/redfen/redfanapp/LoginActivity.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package redfen.redfanapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * 로그인/ 회원가입하는 엑티비티입니다.
@@ -9,9 +12,21 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText inputEmail;
+    private EditText inputPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
     }
+
+    public void  onLoginClick(View view){
+        Intent goToMain = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(goToMain);
+    }
+
 }
