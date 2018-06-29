43308ed1adedccd9445d5787ec98ecd899ee0004
Mingyu Jo <owlsogul@naver.com>
2018-06-30 01:49:45 +0900

    and con: 채널 컨트롤러에 로드되었는지 플래그 추가

---
+++ RedFanApp/app/src/main/java/redfen/redfanapp/model/Video.java
@@ -0,0 +1,8 @@
+package redfen.redfanapp.model;
+
+/**
+ * Created by start on 2018-06-30.
+ */
+
+public class Video {
+}
--- RedFanApp/app/src/main/java/redfen/redfanapp/model_controller/ChannelController.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/model_controller/ChannelController.java
@@ -3,6 +3,7 @@ package redfen.redfanapp.model_controller;
 import redfen.redfanapp.model.Channel;
 
 /**
+ * 채널을 관리하는 컨트롤러 객체. 싱글톤으로 제공된다.
  * Created by JoMingyu on 2018-06-30.
  */
 
@@ -15,7 +16,11 @@ public class ChannelController {
         return instance;
     }
 
-    private Channel channel;
+    private Channel channel; // 로드된 채널에 대한 정보
+    private boolean isLoaded = false; // 채널이 로드 되었는지 알려주는 플래그
+
     public Channel getChannel() {return channel;}
     public void setChannel(Channel channel) {this.channel = channel;}
+    public boolean isLoaded() {return isLoaded;}
+    public void setLoaded(boolean loaded) {isLoaded = loaded;}
 }
