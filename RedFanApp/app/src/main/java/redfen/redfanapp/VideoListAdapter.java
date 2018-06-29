2d7d661d3e2fe7058ea3b11716300f7ca39aec7a
Mingyu Jo <owlsogul@naver.com>
2018-06-29 20:43:55 +0900

    and gui: 비디오 리스트 아이템 그래픽 레이아웃  구현 중

--- RedFanApp/app/src/main/java/redfen/redfanapp/VideoListAdapter.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/VideoListAdapter.java
@@ -1,12 +1,27 @@
 package redfen.redfanapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * 비디오의 리스트를 관리하는 어뎁터 클래스
@@ -47,11 +62,49 @@ public class VideoListAdapter extends BaseAdapter {
         if(convertView==null){
             convertView=inflater.inflate(layout,parent,false);
         }
-        VideoItem listviewitem = itemList.get(position);
-        /*mageView icon=(ImageView)convertView.findViewById(R.id.imageview);
-        icon.setImageResource(listviewitem.getIcon());
-        TextView name=(TextView)convertView.findViewById(R.id.textview);
-        name.setText(listviewitem.getName());*/
+        final VideoItem listviewitem = itemList.get(position);
+
+        final ImageView thumbnail = (ImageView) convertView.findViewById(R.id.imgThumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.txtVideoTitle);
+        TextView percent = (TextView) convertView.findViewById(R.id.txtPercent);
+        ProgressBar progressBar = ((ProgressBar) convertView.findViewById(R.id.barPercent));
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL thumbUrl = new URL(listviewitem.getThumbnailUrl());
+                    HttpsURLConnection conn = (HttpsURLConnection) thumbUrl.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    is.close();
+                    thumbnail.setImageBitmap(bitmap);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        title.setText(listviewitem.getTitle());
+
+        int percentage = (int)((100.0*listviewitem.getLike())/(listviewitem.getLike() + listviewitem.getUnlike()));
+        percent.setText(percentage+"%");
+        ImageView imgGap = (ImageView) convertView.findViewById(R.id.imgGap);
+
+        int base = (int)imgGap.getY();
+        int centerToBase = 0 - ((ImageView) convertView.findViewById(R.id.imgBallon)).getLayoutParams().width/2;
+        int baseToFront = ((ImageView) convertView.findViewById(R.id.imgUp)).getLayoutParams().width;
+        int frontToPercent = (int)(progressBar.getLayoutParams().width/100.0*percentage);
+        System.out.printf("%d %d %d %d\n", base, centerToBase, baseToFront, frontToPercent);
+        imgGap.getLayoutParams().width = base + centerToBase + baseToFront + frontToPercent;
+        imgGap.setLayoutParams(imgGap.getLayoutParams());
+
+        progressBar.setProgress(percentage);
         return convertView;
     }
 }
