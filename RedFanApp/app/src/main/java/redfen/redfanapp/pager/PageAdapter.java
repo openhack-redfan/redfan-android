2b00c882ce5efe5e791d60d3dd52dd99eb0161e3
codeknk <codeknk@github.com>
2018-06-30 05:47:53 +0900

    주석, id, 수정

--- RedFanApp/app/src/main/java/redfen/redfanapp/pager/PageAdapter.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/pager/PageAdapter.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 import com.tsengvn.typekit.TypekitContextWrapper;
 
+import redfen.redfanapp.model.Channel;
+
 /**
  * 메인 뷰에 있는 페이지 슬라이더를 위한 어뎁터입니다.
  * Created by JoMingyu on 2018-06-29.
@@ -15,12 +17,21 @@ import com.tsengvn.typekit.TypekitContextWrapper;
 
 public class PageAdapter extends FragmentPagerAdapter {
 
+    private Channel mChannel;
     private final int size = 4; // page 의 개수를 수동으로 조정
     private Fragment curFragment = new Fragment();
 
+    //슬라이드1. 채널의 기본정보를 보여줍니다.
+    //채널이름, 채널썸네일주소, 구독자수, 비디오수, 채널개설일
     private Fragment channelinfoFragment1 = new ChannelInfoFragment();
+    //슬라이드2. 채널의 총 뷰 수를 보여줍니다.
+    //채널 총 뷰
     private Fragment totalViewFragment2 = new TotalViewFragment().setData("100K views");
+    //슬라이드3. 채널 구독자의 성비를 보여줍니다.
+    //구독자의 성비, 구독자의 연령대
     private Fragment genderViewFragment3 = new GenderViewFragment();
+    //슬라이드4. 채널의 애독자 수를 보여줍니다.
+    //채널 애독자 수
     private Fragment fanViewFragment4 = new FanViewFragment();
 
     public PageAdapter(FragmentManager fm) {
@@ -51,6 +62,10 @@ public class PageAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return size;
+    }
+
+    public void sendChannelInfo(Channel channel){
+        mChannel = channel;
     }
 
 }
