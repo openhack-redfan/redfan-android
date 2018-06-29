98caeb9464f75bcc27cfa88b207e8e1bf12fac4b
Mingyu Jo <owlsogul@naver.com>
2018-06-29 20:43:26 +0900

    and gui: 비디오 리스트 아이템 객체 구현

--- RedFanApp/app/src/main/java/redfen/redfanapp/VideoItem.java
+++ RedFanApp/app/src/main/java/redfen/redfanapp/VideoItem.java
@@ -1,8 +1,33 @@
 package redfen.redfanapp;
 
 /**
- * Created by start on 2018-06-29.
+ * 비디오 리스트의 아이템의 정보를 저장하고 있는 객체입니다.
+ * Created by JoMingyu on 2018-06-29.
  */
 
 public class VideoItem {
+
+    private String title;
+    private String thumbnailUrl;
+
+    private int like;
+    private int unlike;
+
+    public VideoItem(String title, String thumbnailUrl, int like, int unlike){
+        this.title = title;
+        this.thumbnailUrl = thumbnailUrl;
+        this.like = like;
+        this.unlike = unlike;
+    }
+
+    public String getTitle(){return this.title;}
+    public void setTitle(String title){this.title = title;}
+    public String getThumbnailUrl(){return this.thumbnailUrl;}
+    public void setThumbnailUrl(String thumbnailUrl){this.thumbnailUrl = thumbnailUrl;}
+    public int getLike(){return this.like;}
+    public void setLike(int like){this.like = like;}
+    public int getUnlike(){return this.unlike;}
+    public void setUnlike(int unlike){this.unlike = unlike;}
+
+
 }
